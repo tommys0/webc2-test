c406849768d37d1f1c923f8b27871bb9fc94b794
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-01-23 12:43:58 +0100

    Todos pole

---
+++ script.js
@@ -0,0 +1,28 @@
+const todos = [
+    {
+        title: 'WAR Ukol',
+        text: 'Dodělat domácí úkol z reactu na useState Movies',
+        done: false,
+    },
+    {
+        title: 'WEB Test, learning',
+        text: 'Test z nějakých zbytečných věcí, o kterých jsem nevěděl a píšu ho protože druhá skupina to měla psát',
+        done: true,
+    },
+    {
+        title: 'Mamka narozeniny, darek koupit',
+        text: 'Zajít koupit dárek pro mamku na 45. maroziny, koupit do 25.1.',
+        done: false,
+    },
+    {
+        title: 'Koupit letenky',
+        text: 'Koupit letenky do Milána na 27.1.2024 pro 4 osoby a zarezervovat místo v restauraci',
+        done: true,
+    },
+    {
+        title: 'S4T 3D prototype',
+        text: 'Udělat 3D prototyp na S4T + schůze s Patrikem',
+        done: false,
+    },
+]
+
